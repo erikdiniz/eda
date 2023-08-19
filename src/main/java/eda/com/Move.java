e643cd567a2ce8c2abf6fb5bab5d60899d4ba8f5
Erik Diniz <erikdinizbeserra@gmail.com>
2023-08-19 19:55:53 -0300

    "Move" adicionado

---
+++ src/main/java/eda/com/Move.java
@@ -0,0 +1,30 @@
+package eda.com;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+class Move {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        String[] array = sc.nextLine().split(" ");
+
+        for (int i = 0; i < array.length-1; i++){
+            int j = i+1;
+            boolean troca = false;
+
+            if (Integer.parseInt(array[i]) > Integer.parseInt(array[j])){
+                troca = true;
+                swap(array, i, j);
+                i = -1;                
+                System.out.println(Arrays.toString(array));
+            }
+        }
+    }
+    
+    private static void swap(String[] v, int i, int j){
+        String aux = v[i];
+        v[i] = v[j];
+        v[j] = aux;
+    }
+}
