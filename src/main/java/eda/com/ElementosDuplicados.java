1a261be5c59b46069c1e339bc7f9822de4f1248e
Erik Diniz <erikdinizbeserra@gmail.com>
2023-08-20 11:21:13 -0300

    "Elementos duplicados" adicionado

---
+++ src/main/java/eda/com/ElementosDuplicados.java
@@ -0,0 +1,21 @@
+package eda.com;
+
+import java.util.Scanner;
+
+class ElementosDuplicados {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        String[] v = sc.nextLine().split(" ");
+        boolean rep = false;
+
+        for (int i = 0; i < v.length-1; i++){
+            for (int j = i+1; j < v.length; j++){
+                if (v[i].equals(v[j])){
+                    rep = true;
+                }
+            }
+        }
+        System.out.println(rep);
+    }
+}
